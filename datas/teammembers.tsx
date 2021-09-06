1c9884ed10ef2432b8fea3cbd9cb055bb60993e0
memrearal <memrearal@gmail.com>
2021-09-07 02:40:49 +0300

    rave

---
+++ datas/teammembers.tsx
@@ -0,0 +1,47 @@
+export default [
+	{
+		"name": "İBRAHİM KİPER",
+		"job": "CO-FOUNDER",
+		"avatar": require('../images/team-members/kiper.webp')
+	},
+	{
+		"name": "FIRAT DENİZ MÜFTÜOĞLU",
+		"job": "CO-FOUNDER",
+		"avatar": require('../images/team-members/muftuoglu.webp')
+	},
+	{
+		"name": "ALEYNA ATEŞ",
+		"job": "Art Director / Visual Designer",
+		"avatar": require('../images/team-members/ates.webp')
+	},
+	{
+		"name": "ALP YÜKSEL",
+		"job": "Art Director / Motion Graphics Designer",
+		"avatar": require('../images/team-members/yuksel.webp')
+	},
+	{
+		"name": "KUBİLAY KOÇDEMİR",
+		"job": "IT Manager",
+		"avatar": require('../images/team-members/kocdemir.webp')
+	},
+	{
+		"name": "ZEHRA AYTEKİN",
+		"job": "Brand Representative",
+		"avatar": require('../images/team-members/aytekin.jpg')
+	},
+	{
+		"name": "SERENAY BAL",
+		"job": "PR Manager",
+		"avatar": require('../images/team-members/bal.jpg')
+	},
+	{
+		"name": "ÖMER GÜLKAN",
+		"job": "Music Director",
+		"avatar": require('../images/team-members/gulkan.webp')
+	},
+	{
+		"name": "BATUHAN BAŞ",
+		"job": "Visual Designer / 3D Artist",
+		"avatar": require('../images/team-members/bas.jpg')
+	}
+]
